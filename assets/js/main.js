839f195002cc995452a8b7fbbd48ac678171916a
jamesfelicitas <jamesivanfelicitas>
2026-01-13 21:58:50 +0800

    implement dynamic copyright year via js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,5 @@
 console.log("Portfolio loaded.");
 console.log("Developer: James Felicitas");
+
+// 1. Dynamic Year in Footer
+document.getElementById('year').textContent = new Date().getFullYear();
