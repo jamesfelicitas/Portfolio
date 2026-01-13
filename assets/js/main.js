95254cd3c07bfec6d958507224111bd13b319a25
jamesfelicitas <jamesivanfelicitas>
2026-01-14 00:33:28 +0800

    integrate formspree for working contact form

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,8 @@ console.log("Developer: James Felicitas");
 // 1. Dynamic Year in Footer
 document.getElementById('year').textContent = new Date().getFullYear();
 
+// REMOVE OR COMMENT OUT THIS BLOCK SO FORMSPREE WORKS
+/*
 document.getElementById('contactForm').addEventListener('submit', function(event) {
     event.preventDefault(); // Stop page reload
     
@@ -14,3 +16,4 @@ document.getElementById('contactForm').addEventListener('submit', function(event
     this.reset();
 
     });
+*/
